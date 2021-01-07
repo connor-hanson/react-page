33b097e47bfd14e3461ca6041aca5eab1b354f2a
Connor Hanson <cbhanson2@wisc.edu>
2021-01-06 22:56:25 -0600

    buttons and colors

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+To Build/Deploy: <npm run deploy>
+
 ### `yarn start`
 
 Runs the app in the development mode.\
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class DownArrow extends React.Component {
   // move down to next slide
   handleClick(nextY) {
     window.scroll({
-       top: nextY + 1,
+       top: nextY,
         left: 0,
         behavior: 'smooth'
       });
@@ -24,7 +24,7 @@ class DownArrow extends React.Component {
       <div className="scroll-arrow" onClick={() => this.handleClick(this.props.nextY)}>
           <img width="32" 
           alt="Arrow-down" 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Arrow-down.svg/512px-Arrow-down.svg.png"/>
+          src="https://upload.wikimedia.org/wikipedia/commons/2/24/White_arrow_down.svg"/>
       </div>
     );
   }
@@ -59,17 +59,127 @@ class Welcome extends React.Component {
 class Intro extends React.Component {
   constructor(props) {
     super(props);
-    this.switchToRef = React.createRef();
+    this.handleCard1Click = this.handleCard1Click.bind(this);
+    this.handleCard2Click = this.handleCard2Click.bind(this);
+    this.handleCard3Click = this.handleCard3Click.bind(this);
+    this.handleCardExit = this.handleCardExit.bind(this);
+
+    this.state = {
+      topY: 0,
+      bottomY: 2*window.innerHeight,
+      isBlurred: false,
+      ExpandableCard1Visible: false,
+      ExpandableCard2Visible: false,
+      ExpandableCard3Visible: false
+    }
+  }
+
+  // handle opening and closing cards
+  handleCard1Click() {
+    this.setState({
+      isBlurred:true,
+      ExpandableCard1Visible: true
+    });
+    document.getElementById("blur-content").style.filter = "blur(5px)";
+    document.getElementById("ex-card-1").style.display = "block";
+  }
+
+  handleCard2Click() {
+    this.setState({
+      isBlurred:true,
+      ExpandableCard2Visible: true
+    });
+    document.getElementById("blur-content").style.filter = "blur(5px)";
+    document.getElementById("ex-card-2").style.display = "block";
+  }
+
+  handleCard3Click() {
+    this.setState({
+      isBlurred:true,
+      ExpandableCard3Visible: true
+    });
+    document.getElementById("blur-content").style.filter = "blur(5px)";
+    document.getElementById("ex-card-3").style.display = "block";
+    document.getElementById("ex-card-3").style.margin = "auto";
+  }
+
+  handleCardExit() {
+    if (this.state.isBlurred) {
+      this.setState({
+        isBlurred: false,
+        ExpandableCard1Visible: false,
+        ExpandableCard2Visible: false,
+        ExpandableCard3Visible: false
+      });
+      document.getElementById("blur-content").style.filter = "none";
+      let hiddenCards = document.getElementsByClassName("expanded-card");
+
+      // hide all cards again, so we don't need to know which is open
+      for (var i = 0; i < hiddenCards.length; ++i) {
+        hiddenCards[i].style.display = "none";
+      }
+    }
   }
 
   render() {
     return (
-      <div className="intro-page">
-        <p>Some Intro</p>
+      <div className="intro-page" onClick={this.handleCardExit}>
 
-        <DownArrow />
+        <div id="blur-content">
+          <h1>About Me</h1>
+          <p>UW Madison senior graduting with a CS major and a French certificate. I am a talented programmer with experience in a variety of languages (C, C++, Java, JS, Python, PHP, CSS, HTML) and frameworks/templating languages (React.js, Jinja2, Flask, HTML). I am a developer who likes developing both solo projects (complete freedom!!) as well as working with one or more other developers (Brings out my strengths and makes me a better dev).</p>
+          <div className="row">
+            <div className="col-3">
+              <div className="clickable-card" onClick={this.handleCard1Click}>
+                <h3>Languages and Skills!</h3>
+
+                <p>I know C, C++idmnsxzopdxs
+                  iukj,dmcszds
+
+                  ebdhskjnaxdes
+                  poppuhijdesxddcsib
+                  deslladfdsassfewsaxz
+                  uvhkjfdsaqdfvrgfedwsz
+                  uhkjl;[pfedsxwdfreds]
+                </p>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="clickable-card" onClick={this.handleCard2Click}>
+                <h3>Work Experience</h3>
+                <p>Pop</p>  
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="clickable-card" onClick={this.handleCard3Click}>
+                <h3>Hobbies 'n Things</h3>
+                <p>heroin</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="expanded-card" id="ex-card-1">Expand Dong 1</div>
+        <div className="expanded-card" id="ex-card-2">Expand Dong 2</div>
+        <div className="expanded-card" id="ex-card-3">Expand Dong 3</div>
+        
+
+        <DownArrow nextY={this.state.bottomY}/>
       </div>
     ); 
+  }
+}
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <div className="projects-page">
+        <p>My Projects</p>
+      </div>
+    );
   }
 }
 
@@ -87,6 +197,7 @@ function App() {
   <div className="App">
     <Welcome />
     <Intro />
+    <Projects />
   </div>
   );
 }
